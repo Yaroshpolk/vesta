e485fd9f1be2a907f76ece199c0a7a079ad3af71
Yaroshevich Vlad <yaroshevich00@mail.ru>
2021-01-18 19:21:11 +0300

    footer mobile done

--- blocks/mobile-menu/mob-menu.js
+++ blocks/mobile-menu/mob-menu.js
@@ -1,14 +1,32 @@
 $(document).ready(function(){
-    let flag = 0;
-    $(".navbar__menu-ic").click(function (){
-        if (flag == 0){
-            $(".mobile-menu").css({ "height":"500px"});
-            $(".header .navbar__block_left, .header .navbar__phone").css("opacity",".4");
-            flag++;
-        } else {
-            $(".mobile-menu").css({ "height":"0"});
-            $(".header .navbar__block_left, .header .navbar__phone").css("opacity","1");
-            flag--;
-        }
+    let active = false;
+ function menu(position) {
+
+            if (active === false){
+                $(position + " .mobile-menu").css({ "height":"510px"});
+                if (position === ".header"){
+                    $(position + " .navbar__block_left, .header .navbar__phone").css("opacity",".4");
+                }
+                active = true;
+                console.log(active);
+            } else{
+                $(position + " .mobile-menu").css({ "height":"0"});
+                if (position === ".header"){
+                    $(position + " .navbar__block_left, .header .navbar__phone").css("opacity","1");
+                }
+                active = false;
+                console.log(active);
+            }
+
+            return active;
+    }
+
+    $(".footer .navbar__menu-ic").click(function (){
+        menu(".footer");
     });
+
+    $(".header .navbar__menu-ic").click(function (){
+        menu(".header");
+    });
+
 })
