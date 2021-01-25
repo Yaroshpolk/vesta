c2c2899302f913c42eeec48ebfd04ea6eadde259
Yaroshevich Vlad <yaroshevich00@mail.ru>
2021-01-25 16:37:57 +0300

    item mobile adaptive done

--- blocks/mobile-menu/mob-menu.js
+++ blocks/mobile-menu/mob-menu.js
@@ -1,30 +1,35 @@
 $(document).ready(function(){
-    let active = false;
- function menu(position) {
 
-            if (active === false){
+ function menu(position, active) {
+
+            if (active == false){
                 $(position + " .mobile-menu").css({ "height":"510px"});
                 if (position === ".header"){
                     $(position + " .navbar__block_left, .header .navbar__phone").css("opacity",".4");
                 }
                 active = true;
+
             } else{
                 $(position + " .mobile-menu").css({ "height":"0"});
                 if (position === ".header"){
                     $(position + " .navbar__block_left, .header .navbar__phone").css("opacity","1");
                 }
                 active = false;
+
             }
 
             return active;
     }
 
     $(".footer .navbar__menu-ic").click(function (){
-        menu(".footer");
+        menu(".footer", $(this).data("toggle"));
+        $(this).data("toggle", !$(this).data("toggle"));
     });
 
     $(".header .navbar__menu-ic").click(function (){
-        menu(".header");
+        console.log($(this).data("toggle"));
+        menu(".header", $(this).data("toggle"));
+        $(this).data("toggle", !$(this).data("toggle"));
     });
 
 })
